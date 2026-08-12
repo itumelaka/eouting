df77185a4fc4a6cce8286a02b9b32fe8d8351630
BurnDVS <burn.kajang@gmail.com>
2026-08-12 11:53:27 +0800

    chore: bump frontend cache revision to r5

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "eouting-cache-v2.2.0-r4";
+const CACHE_NAME = "eouting-cache-v2.2.0-r5";
 
 const APP_SHELL_ASSETS = [
   "./",
   "./index.html",
-  "./assets/style.css?v=2.2.0-r4",
-  "./assets/app.js?v=2.2.0-r4",
+  "./assets/style.css?v=2.2.0-r5",
+  "./assets/app.js?v=2.2.0-r5",
   "./assets/pwa-logo.png",
   "./assets/eouting-header-logo.png",
   "./assets/icons/icon-192.png",
